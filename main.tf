782ed0b16bf65f54672eddb8541fab3e0c73bc3a
Proxy <emergenceproxy@gmail.com>
2026-06-25 20:50:14 -0400

    Correct ami id

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ytcdl-VM"{
 	count = 1
-	ami = "i-0506008b7824b0527"
+	ami = "ami-060988b0dff2faa7c"
 	instance_type = "t3.micro"
 	key_name = "ProxyRustyServer_KeyPair_2"
 	security_groups = ["sg-068ce2334ae207a39"]
@@ -15,7 +15,7 @@ resource "aws_instance" "ytcdl-VM"{
 
 resource "aws_instance" "rusty-VM"{
 	count = 0
-	ami = "i-0506008b7824b0527"
+	ami = "ami-060988b0dff2faa7c"
 	instance_type = "t3.micro"
 	tags = {
 		Name = "proxy_rusty_vm"
